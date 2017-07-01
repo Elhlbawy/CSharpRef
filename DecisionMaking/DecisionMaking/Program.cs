4dfe103ae684e0084044deb97d0ef342491f3122
Abo Juwayriya Elhelbawy <drabosafi@gmail.com>
2017-07-01 23:55:26 +0300

    Making Decision With CSharp
    it`s an sample about how to make decision with csharp lang VS2017

---
+++ DecisionMaking/DecisionMaking/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecisionMaking;
+
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Excute if statement
+            // Grade(); you shoud uncomment taht line
+            
+            // Excute if and default else
+            // GradeIfelse(); you shoud uncomment taht line
+            
+            // Excute if more conditions and default else
+            //  GradeIfelseif(); you shoud uncomment taht line
+
+            Console.ReadKey(); // to stop Console Flashing 
+        }
+
+        // There are Many ways to make decision in csharp
+
+        //If Statement for one Condition
+        static void Grade()
+        {
+            Console.WriteLine("Enter your Grade: ");
+            string grade = Console.ReadLine();
+
+            if (grade == "A")
+            {
+                Console.WriteLine("Your Grade Is: Excellent");
+            }
+        }
+        //If Statement for one Condition and make a default case with esle
+        static void GradeIfelse()
+        {
+            Console.WriteLine("Enter your Grade: ");
+            string grade = Console.ReadLine();
+
+            if (grade == "A")
+            {
+                Console.WriteLine("Your Grade Is: Excellent");
+            }else
+            {
+                //Here is the defualt, if condition is false
+                Console.WriteLine("Sorry, Enter right Grade");
+            }
+        }
+       
+        //If Statement for more Conditions and make a default case with esle
+        static void GradeIfelseif()
+        {
+            Console.WriteLine("Enter your Grade: ");
+            string grade = Console.ReadLine();
+
+            if (grade == "A")
+            {
+                Console.WriteLine("Your Grade Is: Excellent");
+            }else if (grade=="B")
+            {
+                Console.WriteLine("Your Grade Is: Very Good");
+            }
+            else if (grade == "C")
+            {
+                Console.WriteLine("Your Grade Is: Good");
+            }
+            else if (grade == "D")
+            {
+                Console.WriteLine("Your Grade Is: Sorry");
+            }
+            else
+            {
+                //Here is the defualt, if condition is false
+                Console.WriteLine("Sorry, Enter right Grade");
+            }
+        }
+
+    }
+
+}
---
+++ DecisionMaking/DecisionMaking/Switch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaking
+{
+    class Switch
+    {
+        // making decision with switch statement
+       
+
+        public void switchSt()
+        {
+            Console.WriteLine("Plsm Enter your Degree A, B, C, D ");
+            string userDegree = Console.ReadLine(); //to recive user input
+            switch (userDegree) //condition
+            {
+                case "A":
+                    Console.WriteLine("Cong, You are Excellent");
+                    break;
+                case "B":
+                    Console.WriteLine("Cong, You are Very Good");
+                    break;
+                case "C":
+                    Console.WriteLine("Cong, You are  Good");
+                    break;
+                case "D":
+                    Console.WriteLine("Cong, You are  Good");
+                    break;
+                default:
+                    Console.WriteLine("Sorry, Have a good luck");
+                    break;
+
+
+
+            }
+        }
+    }
+}
